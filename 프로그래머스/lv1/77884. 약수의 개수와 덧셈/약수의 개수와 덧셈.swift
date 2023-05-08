bc8389e53489dc74e84418d7db25d653c2d4af81
Reswo._ <102791216+HongSJae@users.noreply.github.com>
2023-05-08 09:01:28 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 5.54 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.swift
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.swift
@@ -1,14 +1,5 @@
 import Foundation
 
 func solution(_ left:Int, _ right:Int) -> Int {
-    var sum = 0
-    (left...right).map { n in (1...n).filter { n % $0 == 0 }}
-        .forEach { list in
-            if list.count % 2 == 0 {
-                sum += list.last!
-            } else {
-                sum += -list.last!
-            }
-        }
-    return sum
+    return (left...right).map { n in (1...n).filter { n % $0 == 0 }.count % 2 == 0 ? n : -n }.reduce(0, +)
 }
